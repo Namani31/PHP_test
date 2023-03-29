8b32be318272674cb892f5b123ceb0cefce60e6f
YunJi <kimyunji9272@gmail.com>
2023-03-29 18:01:16 +0900

    add insert variable and query

--- login.php
+++ login.php
@@ -4,20 +4,23 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "mycat";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$nm_conn = new mysqli($servername, $username, $password, $dbname);
+if ($nm_conn->connect_error) {
+  die("Connection failed: " . $nm_conn->connect_error);
 }
 
 // 폼 데이터 수집 및 쿼리 작성
 $name = $_POST['name'];
 $email = $_POST['email'];
-$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
-$sql = "SELECT * FROM users WHERE name='$name' AND email='$email'";
+$nm_sql_insert = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+$nm_sql_result = "SELECT * FROM users WHERE name='$name' AND email='$email'";
 
+if ($nm_conn->query($nm_sql_insert) === FALSE) {
+  echo "등록 중 오류가 발생했습니다.: " . $conn->error;
+}
 
-// 쿼리 실행 및 결과 처리 (select문 확인용 쿼리)
-$result = $conn->query($sql);
+// 쿼리 실행 및 결과 처리
+$result = $nm_conn->query($nm_sql_result);
 if ($result->num_rows > 0) {
   // 로그인 성공
   echo "Login success!";
@@ -26,13 +29,6 @@ if ($result->num_rows > 0) {
   echo "Invalid username or password";
 }
 
-/* (insert 문 확인용 쿼리)
-if ($conn->query($sql) === TRUE) {
-  echo "사용자가 성공적으로 등록되었습니다.";
-} else {
-  echo "등록 중 오류가 발생했습니다.: " . $conn->error;
-} */
-
 // 데이터베이스 연결 종료
-$conn->close();
+$nm_conn->close();
 ?>
